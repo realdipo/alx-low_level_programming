3bc2395455edcb76d6cdaa235f62b4b85a0a2ab7
realdipo <dahrealdipo@gmail.com>
2023-06-19 23:00:07 +0100

    Introduction to c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
-*main- key entry point
-*put- to output a script
-*return: to always 0
-*Hello world,C style
-*/
+  *main- main entry point
+  *return: always 0(succcessful)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
