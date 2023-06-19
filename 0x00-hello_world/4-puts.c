07aa6db4a8d7478d77a54ec48626deaaf85a0f2d
realdipo <dahrealdipo@gmail.com>
2023-06-19 20:16:43 +0100

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main- key entry point
+*put- to output a script
+*return: to always 0
+*Hello world,C style
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
