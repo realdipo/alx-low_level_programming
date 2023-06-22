3cfabc8dd9b27ffd933f73eff7c08cd815a90908
realdipo <dahrealdipo@gmail.com>
2023-06-22 05:58:51 +0100

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _abs - Does the absolute value of an integer
+  * @1: integer input number
+  * Return: absolute value
+  */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return(i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
