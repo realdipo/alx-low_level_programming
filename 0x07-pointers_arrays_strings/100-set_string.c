e02a56853aff4617348cbf935b38980b90f974a0
realdipo <dahrealdipo@gmail.com>
2023-07-04 06:28:20 +0100

    Adv task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * set_string - is a point of entry
+  * @s: first input
+  * @to: second input
+  * return: is always 0 (success)
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
