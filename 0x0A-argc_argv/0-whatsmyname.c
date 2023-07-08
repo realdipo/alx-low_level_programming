4c3211c03694cab59f7168869ec6f7f87c4dd280
realdipo <dahrealdipo@gmail.com>
2023-07-08 14:43:39 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the name
+ * @argc: count of present arguments
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
